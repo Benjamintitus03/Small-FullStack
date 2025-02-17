69ced39ee336c8fa78c91955d06d4c51414d4549
Debian <azel.padron@gmail.com>
2025-02-17 06:22:31 +0000

    Pushing all files from the htmldocs onto the master branch

---
+++ LAMPAPI/Login.php
@@ -0,0 +1,62 @@
+<?php
+
+	ini_set('display_erros', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+
+	$inData = getRequestInfo();
+	
+	$id = 0;
+	$username = "";
+
+	$conn = new mysqli("localhost", "root", "WeLoveCOP4331", "contact_manager"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT ID,username,password FROM users WHERE username=? AND password=?");
+		$stmt->bind_param("ss", $inData["username"], $inData["password"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if( $row = $result->fetch_assoc()  )
+		
+		{
+			returnWithInfo($row['username'], $row['ID']);
+		}
+		else
+		{
+			returnWithError("Username not found or password not correct.");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"username":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $username, $id )
+	{
+		$retValue = '{"id":' . $id . ',"username":"' . $username . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
---
+++ LAMPAPI/editUsername.php
@@ -0,0 +1,71 @@
+
+<?php
+$inData = getRequestInfo();
+
+// Connect to the database (need to find out if we have a user credential to connect to it first...)
+$conn = new mysqli("localhost", "root", "KkRzm9FjDZ@g", "contact_manager");
+// Check if the connection was successful
+if ($conn->connect_error)
+{
+    returnWithError($conn->connect_error);
+}
+// If we can connect, continue.
+else
+{
+    // Check if the login already exists
+    $stmt = $conn->prepare("SELECT id FROM users WHERE username=?");
+    $stmt->bind_param("s", $inData["newUsername"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+    
+    // If information successfully returns a user, return error msg "user already exists"
+    if ($result->fetch_assoc())
+    {
+        returnWithError("Username already exists");
+    }
+    // Else, we will use the input to edit a username in the DB
+    else
+    {
+        // Insert new username for the ID provided
+        $stmt = $conn->prepare("UPDATE users SET username=? WHERE id=?");
+        $stmt->bind_param("si", $inData["newUsername"], $inData["id"]);
+
+        if ($stmt->execute()) 
+        {
+            returnWithSuccess("Username updated successfully!");
+        }
+        else
+        {
+            returnWithError("Error updating your username, please try again later.");
+        }
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+// possibly need to edit this function ; unsure if it is going to send the correct info out (this is essentially returnWithInfo from login.php)
+function returnWithSuccess($msg)
+{
+    $retValue = '{"message":"' . $msg . '","error":""}';
+    sendResultInfoAsJson($retValue);
+}
+?>
