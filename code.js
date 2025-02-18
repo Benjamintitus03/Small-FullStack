76317389fb1a1719aa540319a68233c0c583bc37
Brandon Zabawa <brandonzabawa123@gmail.com>
2025-02-17 21:30:02 -0500

    For code.js I commented out the entire script to test what did and didnt work to allow userInput for the buttons to work properly. I then started making tailored scripts under each HTML file to focus on basic button input, and used event-action listeners to ensure functionality. I confirmed the button was inputted by including a debug log which I later got rid of, added trim functions for the fill-in field sections for the user input, redirected it to different pages, and ensured at bare minimum that the overall UI is working fine.

--- code.js
+++ code.js
@@ -1,158 +1,158 @@
-// Added eventListener to load up the DOM before other functions get executed.
-document.addEventListener('DOMContentLoaded', function () { 
-  const urlBase = 'http://proctest.titusknights.help/LAMPAPI';
-  const extension = 'php';
+// // Added eventListener to load up the DOM before other functions get executed.
+// document.addEventListener('DOMContentLoaded', function () { 
+//   const urlBase = 'http://proctest.titusknights.help/LAMPAPI';
+//   const extension = 'php';
 
-  let userId = 0;
-  let firstName = "";
-  let lastName = "";
+//   let userId = 0;
+//   let firstName = "";
+//   let lastName = "";
 
-  // ------------------- LOGIN -------------------
-  // window.doLogin = function () {
-  //   console.log("doLogin triggered");
-  //   userId = 0;
-  //   firstName = "";
-  //   lastName = "";
+//   // ------------------- LOGIN -------------------
+//   // window.doLogin = function () {
+//   //   console.log("doLogin triggered");
+//   //   userId = 0;
+//   //   firstName = "";
+//   //   lastName = "";
 
-  //   let login = document.getElementById("loginName").value;
-  //   let password = document.getElementById("loginPassword").value;
+//   //   let login = document.getElementById("loginName").value;
+//   //   let password = document.getElementById("loginPassword").value;
 
-  //   document.getElementById("loginResult").innerHTML = "";
+//   //   document.getElementById("loginResult").innerHTML = "";
 
-  //   let tmp = { login: login, password: password };
-  //   let jsonPayload = JSON.stringify(tmp);
-  //   let url = urlBase + '/Login.' + extension;
+//   //   let tmp = { login: login, password: password };
+//   //   let jsonPayload = JSON.stringify(tmp);
+//   //   let url = urlBase + '/Login.' + extension;
 
-  //   let xhr = new XMLHttpRequest();
-  //   xhr.open("POST", url, true);
-  //   xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-  //   // try {
-  //   //   xhr.onreadystatechange = function () {
-  //   //     if (this.readyState == 4 && this.status == 200) {
-  //   //       let jsonObject = JSON.parse(xhr.responseText);
-  //   //       userId = jsonObject.id;
+//   //   let xhr = new XMLHttpRequest();
+//   //   xhr.open("POST", url, true);
+//   //   xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+//   //   // try {
+//   //   //   xhr.onreadystatechange = function () {
+//   //   //     if (this.readyState == 4 && this.status == 200) {
+//   //   //       let jsonObject = JSON.parse(xhr.responseText);
+//   //   //       userId = jsonObject.id;
 
-  //   //       if (userId < 1) {
-  //   //         document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-  //   //         return;
-  //   //       }
+//   //   //       if (userId < 1) {
+//   //   //         document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+//   //   //         return;
+//   //   //       }
 
-  //   //       firstName = jsonObject.firstName;
-  //   //       lastName = jsonObject.lastName;
-  //   //       saveCookie();
-  //   //       window.location.href = "AddContact.html";// window.location.href = "color.html";
-  //   //     }
-  //   //   };
-  //   //   xhr.send(jsonPayload);
-  //   // } catch (err) {
-  //   //   document.getElementById("loginResult").innerHTML = err.message;
-  //   // }
-  // };
-  try{
-  document.getElementById("doLogin").onclick = function () {
-    console.log("doLogin triggered");
-    userId = 0;
-    firstName = "";
-    lastName = "";
+//   //   //       firstName = jsonObject.firstName;
+//   //   //       lastName = jsonObject.lastName;
+//   //   //       saveCookie();
+//   //   //       window.location.href = "AddContact.html";// window.location.href = "color.html";
+//   //   //     }
+//   //   //   };
+//   //   //   xhr.send(jsonPayload);
+//   //   // } catch (err) {
+//   //   //   document.getElementById("loginResult").innerHTML = err.message;
+//   //   // }
+//   // };
+//   try{
+//   document.getElementById("doLogin").onclick = function () {
+//     console.log("doLogin triggered");
+//     userId = 0;
+//     firstName = "";
+//     lastName = "";
 
-    let login = document.getElementById("loginName").value;
-    let password = document.getElementById("loginPassword").value;
+//     let login = document.getElementById("loginName").value;
+//     let password = document.getElementById("loginPassword").value;
 
-    document.getElementById("loginResult").innerHTML = "";
+//     document.getElementById("loginResult").innerHTML = "";
 
-    let tmp = { login: login, password: password };
-    let jsonPayload = JSON.stringify(tmp);
-    let url = urlBase + '/Login.' + extension;
+//     let tmp = { login: login, password: password };
+//     let jsonPayload = JSON.stringify(tmp);
+//     let url = urlBase + '/Login.' + extension;
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    // try {
-    //   xhr.onreadystatechange = function () {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //       let jsonObject = JSON.parse(xhr.responseText);
-    //       userId = jsonObject.id;
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("POST", url, true);
+//     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+//     // try {
+//     //   xhr.onreadystatechange = function () {
+//     //     if (this.readyState == 4 && this.status == 200) {
+//     //       let jsonObject = JSON.parse(xhr.responseText);
+//     //       userId = jsonObject.id;
 
-    //       if (userId < 1) {
-    //         document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-    //         return;
-    //       }
+//     //       if (userId < 1) {
+//     //         document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+//     //         return;
+//     //       }
 
-    //       firstName = jsonObject.firstName;
-    //       lastName = jsonObject.lastName;
-    //       saveCookie();
-    //       window.location.href = "AddContact.html";// window.location.href = "color.html";
-    //     }
-    //   };
-    //   xhr.send(jsonPayload);
-    // } catch (err) {
-    //   document.getElementById("loginResult").innerHTML = err.message;
-    // }
-  };
-}
-catch{
-  console.log("do something");
-}
-  // ------------------- REGISTRATION -------------------
-  window.doRegister = function () {
-    let username = document.getElementById("registerName").value;
-    let password = document.getElementById("registerPassword").value;
+//     //       firstName = jsonObject.firstName;
+//     //       lastName = jsonObject.lastName;
+//     //       saveCookie();
+//     //       window.location.href = "AddContact.html";// window.location.href = "color.html";
+//     //     }
+//     //   };
+//     //   xhr.send(jsonPayload);
+//     // } catch (err) {
+//     //   document.getElementById("loginResult").innerHTML = err.message;
+//     // }
+//   };
+// }
+// catch{
+//   console.log("do something");
+// }
+//   // ------------------- REGISTRATION -------------------
+//   window.doRegister = function () {
+//     let username = document.getElementById("registerName").value;
+//     let password = document.getElementById("registerPassword").value;
 
-    document.getElementById("registerResult").innerHTML = "";
+//     document.getElementById("registerResult").innerHTML = "";
 
-    let tmp = { username: username, password: password };
-    let jsonPayload = JSON.stringify(tmp);
-    let url = urlBase + '/createUser.' + extension;
+//     let tmp = { username: username, password: password };
+//     let jsonPayload = JSON.stringify(tmp);
+//     let url = urlBase + '/createUser.' + extension;
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try {
-      xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-          let jsonObject = JSON.parse(xhr.responseText);
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("POST", url, true);
+//     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+//     try {
+//       xhr.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//           let jsonObject = JSON.parse(xhr.responseText);
 
-          if (jsonObject.error) {
-            document.getElementById("registerResult").innerHTML = jsonObject.error;
-          } else {
-            alert(jsonObject.message);
-            window.location.href = "UpdateContact.html"; // Redirect to login
-          }
-        }
-      };
-      xhr.send(jsonPayload);
-    } catch (err) {
-      document.getElementById("registerResult").innerHTML = err.message;
-    }
-  };
+//           if (jsonObject.error) {
+//             document.getElementById("registerResult").innerHTML = jsonObject.error;
+//           } else {
+//             alert(jsonObject.message);
+//             window.location.href = "UpdateContact.html"; // Redirect to login
+//           }
+//         }
+//       };
+//       xhr.send(jsonPayload);
+//     } catch (err) {
+//       document.getElementById("registerResult").innerHTML = err.message;
+//     }
+//   };
 
-  // ------------------- COOKIE FUNCTIONS -------------------
-  function saveCookie() {
-    let minutes = 20;
-    let date = new Date();
-    date.setTime(date.getTime() + (minutes * 60 * 1000));
-    document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
-  }
+//   // ------------------- COOKIE FUNCTIONS -------------------
+//   function saveCookie() {
+//     let minutes = 20;
+//     let date = new Date();
+//     date.setTime(date.getTime() + (minutes * 60 * 1000));
+//     document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+//   }
 
-  function readCookie() {
-    userId = -1;
-    let data = document.cookie;
-    let splits = data.split(",");
-    for (let i = 0; i < splits.length; i++) {
-      let thisOne = splits[i].trim();
-      let tokens = thisOne.split("=");
-      if (tokens[0] == "firstName") firstName = tokens[1];
-      else if (tokens[0] == "lastName") lastName = tokens[1];
-      else if (tokens[0] == "userId") userId = parseInt(tokens[1].trim());
-    }
-    if (userId < 0) window.location.href = "index.html";
-  }
+//   function readCookie() {
+//     userId = -1;
+//     let data = document.cookie;
+//     let splits = data.split(",");
+//     for (let i = 0; i < splits.length; i++) {
+//       let thisOne = splits[i].trim();
+//       let tokens = thisOne.split("=");
+//       if (tokens[0] == "firstName") firstName = tokens[1];
+//       else if (tokens[0] == "lastName") lastName = tokens[1];
+//       else if (tokens[0] == "userId") userId = parseInt(tokens[1].trim());
+//     }
+//     if (userId < 0) window.location.href = "index.html";
+//   }
 
-  function doLogout() {
-    userId = 0;
-    firstName = "";
-    lastName = "";
-    document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-    window.location.href = "index.html";
-  }
-});
+//   function doLogout() {
+//     userId = 0;
+//     firstName = "";
+//     lastName = "";
+//     document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+//     window.location.href = "index.html";
+//   }
+// });
